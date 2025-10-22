5066c372c8a9049470061cd57370f27e171c1ee7
Aryan-Rathour <aryanrathour066@gmail.com>
2025-10-22 19:17:53 +0530

    changing the path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import CoursesSection from "./CoursesSection";
 import Partners from "./PartnersLogo";
 import CourseTestimonials from "./CourseTestimonials";
 import PartnersCard from "./PartnersCard";
-import Footer from "./Footer";
+import Footer from "./footer";
 
 
 export default function Home() {
